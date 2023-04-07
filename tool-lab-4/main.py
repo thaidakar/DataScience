2831bc1b5e40388a08a64c4fdb87b917927608ad
Sean Brady <sbrady@efilecabinet.com>
2023-04-07 10:39:34 -0600

    model prediction

--- tool-lab-4/main.py
+++ tool-lab-4/main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import wandb
+from wandb.keras import WandbMetricsLogger
+import matplotlib.pyplot as plt
+import cv2
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from starlette.requests import Request
+from starlette.responses import Response
 
+limiter = Limiter(key_func=get_remote_address) # Gets the user's address
 app = FastAPI()
+app.state.limiter = limiter # Define's the api's limiter object
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) # Adds the exception to the api
 
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+@app.get("/base/home")
+@limiter.limit("15/minute")
+async def classify(request:Request, response:Response):
+    return {"api_info": "Sean - Brady's CS280 Image Classifier"}
+
+
+@app.post("/classify/")
+@limiter.limit("5/minute")
+async def classify(request:Request, response:Response, file: UploadFile = File(...)):
+    fileContent = await file.read() # Reads file information from request as a stream of bytes
+    batchedImage = convertFileToBatchedImage(fileContent)
+    label = predictImage(batchedImage)
+    return {"classification": f"{label}"} # Returns the filename as a response.
+
+
+def convertFileToBatchedImage(fileContent):
+  image = Image.open(BytesIO(fileContent)) # Converts file to a python image from a stream of bytes.
+  imageArray = np.asarray(image) #converts image to numpy array
+  smallImage = cv2.resize(imageArray, dsize=(32, 32), interpolation=cv2.INTER_CUBIC) # Resizes the image
+  batchedImage = np.expand_dims(smallImage, axis=0)
+  return batchedImage
+
+def predictImage(batchedImage):
+   model = keras.models.load_model("trainedModel.h5")
+   label = model.predict(batchedImage)
+   return label
